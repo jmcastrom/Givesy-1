42c503eff780da7c38987e9f2b2260655e5cd927
Dayana Arrieta <DayanaArrieta>
2022-02-10 23:43:22 -0500

    cambios en función home

--- movie/views.py
+++ movie/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html', {'name': 'Greg Lim'})
+    return render(request, 'home.html', {'name': 'Dayana Arrieta'})
 
